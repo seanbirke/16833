8ca2fc5159bdf2516e4ea251f3801915f8295875
thomasw2 <thomasw2@andrew.cmu.edu>
2020-09-25 02:02:20 -0400

    implemented basic sensor/ray tracing. needs tuning. check what orientation the map is for ray tracing

--- SensorModel.py
+++ SensorModel.py
@@ -1,11 +1,12 @@
 import numpy as np
 import math
+import sys
 import time
 from matplotlib import pyplot as plt
 from scipy.stats import norm
 import pdb
-
 from MapReader import MapReader
+from rayTrace import rayTrace
 
 class SensorModel:
 
@@ -16,9 +17,41 @@ class SensorModel:
 
     def __init__(self, occupancy_map):
 
-        """
-        TODO : Initialize Sensor Model parameters here
-        """
+        self.map=occupancy_map
+        #note: adjust these later
+        self.stdDevHit=0.01
+        self.lambdaShort=0.01
+        self.measureMax=200
+        self.zHit=0.25
+        self.zShort=0.25
+        self.zMax=0.25
+        self.zRand=0.25
+
+    def pHit(self,zkt,xt,xtLaser):
+        if 0<=zkt and zkt<=self.measureMax:
+            #normalizer is cdf of the norm
+            zktStar = rayTrace(xtLaser, map)
+            zVal=float(zkt-zktStar)/self.stdDevHit
+            normalizer=1/norm.cdf(zVal)
+            return normalizer*norm.pdf(zVal)
+        return 0
+
+    def pShort(self,zkt,xt,xtLaser):
+        zktStar=rayTrace(xtLaser,self.map)
+        if 0<=zkt and zkt<=zktStar:
+            n=1.0/(1-math.exp(-self.lambdaShort*zktStar))
+            return n*self.lambdaShort*math.exp(-self.lambdaShort*zkt)
+        return 0
+
+    def pMax(self,zkt):
+        if zkt==self.measureMax:
+            return 1
+        return 0
+
+    def pRand(self,zkt):
+        if 0<=zkt and zkt < self.measureMax:
+            return 1.0/self.measureMax
+        return 0
 
     def beam_range_finder_model(self, z_t1_arr, x_t1):
         """
@@ -26,7 +59,23 @@ class SensorModel:
         param[in] x_t1 : particle state belief [x, y, theta] at time t [world_frame]
         param[out] prob_zt1 : likelihood of a range scan zt1 at time t
         """
+        xRobot=z_t1_arr[1]
+        yRobot=z_t1_arr[2]
+        thRobot=z_t1_arr[3]
+        xLaser=z_t1_arr[4]
+        yLaser=z_t1_arr[5]
+        thLaser=z_t1_arr[6]
+        q=1
 
+        for k in range(7,187):
+            #compute zkt
+            zkt=z_t1_arr[k]
+            xt=[xRobot,yRobot,thRobot-math.pi/2+(k-7)*math.pi/180]
+            xtLaser = [xLaser,yLaser,thLaser-math.pi/2+(k-7)*math.pi/180]
+            p=self.zHit*self.pHit(zkt,xt,xtLaser)\
+            +self.zShort*self.pShort(zkt,xt,xtLaser)\
+            +self.zMax*self.pMax(zkt)\
+            +self.zRand*self.pRand(zkt)
         """
         TODO : Add your code here
         """
@@ -34,4 +83,6 @@ class SensorModel:
         return q    
  
 if __name__=='__main__':
+    print("bruh")
+    sys.exit(0)
     pass
---
+++ rayTrace.py
@@ -0,0 +1,51 @@
+import math
+#note: assumes map pixels are 10 cm
+#estimates distance to nearest surface by following traj of x until hitting surface in map
+#pos=[x,y,theta] of the LASER, not the robot
+#also adjusts for the ~25cm offset from the actual robot position
+def rayTrace(pos,map):
+    #find current block in map
+    adj=25
+    xSlope=math.cos(pos[2])
+    ySlope=math.sin(pos[2])
+    maxX=map.shape[0]*10;
+    maxY=map.shape[1]*10;
+    #put 25cm adjustment for laser
+    rayPos=[pos[0]-adj*xSlope,pos[1]-adj*ySlope,pos[2]];
+    res=10
+    wallPosX=(maxX-pos[0])
+    wallPosY=(maxY-pos[1])
+    withinX = rayPos[0] > 0 and rayPos[0] < maxX
+    withinY = rayPos[1] > 0 and rayPos[1] < maxY
+    print("bruh%b%b", (withinX, withinY, int(rayPos[0] / 10), int(rayPos[1] / 10), rayPos[0], rayPos[1]))
+    notWall = (withinX and withinY and map[int(rayPos[1] / 10)][int(rayPos[0] / 10)] < 0)
+    while(withinX and withinY and notWall):
+        toNext=0
+        toNextX=0
+        toNextY=0
+        eps=0.00001
+        if(xSlope<eps):
+            toNext=max((rayPos[1]%10)/ySlope , (10 -rayPos[1]%10)/ySlope)
+        else:
+            toNextX=max((rayPos[0]%10)/xSlope , (10 -rayPos[0]%10)/xSlope)
+        if(ySlope<eps):
+            toNext=max((rayPos[0]%10)/xSlope , (10 -rayPos[0]%10)/xSlope)
+        else:
+            toNextY=max((rayPos[1]%10)/ySlope , (10 -rayPos[1]%10)/ySlope)
+        if toNext==0:
+            toNext=min(toNextX,toNextY)
+        #update raypos to border of next grid space
+        newPos=[rayPos[0]+toNext*xSlope, rayPos[1]+toNext*ySlope,rayPos[2]]
+        eps2=0.001
+        #check if still in same grid space due to floats; if is, increment by small amount
+        while(newPos[0]/10==rayPos[0]/10 and newPos[1]/10==rayPos[1]/10):
+            newPos = [newPos[0]+eps2*xSlope, newPos[1]+eps2*ySlope,rayPos[2]]
+
+        #update rayPos
+        rayPos=newPos
+        withinX=rayPos[0]>0 and rayPos[0]<maxX
+        withinY=rayPos[1]>0 and rayPos[1]<maxY
+        print("bruh%b%b", (withinX,withinY,int(rayPos[0]/10),int(rayPos[1]/10),rayPos[0],rayPos[1]))
+        notWall = (withinX and withinY and map[int(rayPos[1]/10)][int(rayPos[0]/10)] < 0)
+        #return ray length
+    return math.sqrt((pos[0]-rayPos[0])**2+(pos[1]-rayPos[1])**2)
