883e3d58bdfbf143f16922b459f72551f142c47c
Sean Birke <seanbirke@Seans-MacBook-Pro-2.local>
2020-09-27 16:43:45 -0400

    Added low_variance_resampler

--- Resampling.py
+++ Resampling.py
@@ -27,17 +27,30 @@ class Resampling:
         return X_bar_resampled
 
     def low_variance_sampler(self, X_bar):
+        # Assumes input X_bar is a numpy array of floats
 
         """
         param[in] X_bar : [num_particles x 4] sized array containing [x, y, theta, wt] values for all particles
         param[out] X_bar_resampled : [num_particles x 4] sized array containing [x, y, theta, wt] values for resampled set of particles
         """
+        X_bar_resampled = []
 
-        """
-        TODO : Add your code here
-        """
-        
-        return X_bar_resampled
+        # Normalize weights on (0,1)
+        X_bar[:,3] = X_bar[:,3]/np.sum(X_bar[:,3])
+
+        M = X_bar.shape[0]
+        r = np.random.uniform(0,1/M)
+        c = X_bar[0,3]
+
+        i = 0
+        for m in range(1, M):
+            U = r + (m - 1) * (1/M)
+            while U > c:
+                i += 1
+                c += X_bar[i,3]
+            X_bar_resampled.append(X_bar[i])
+
+        return np.array(X_bar_resampled)
 
 if __name__ == "__main__":
     pass
