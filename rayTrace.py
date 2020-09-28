da98f18b83f2af5fa5514d82ea48d82e3f7f6053
thomasw2 <thomasw2@andrew.cmu.edu>
2020-09-27 21:37:36 -0400

    made raytrace faster but probably less accurate

--- rayTrace.py
+++ rayTrace.py
@@ -18,37 +18,22 @@ def rayTrace(pos,lAngle,map):
     maxY=map.shape[1]*10
     wallPosX=(maxX-pos[0])
     wallPosY=(maxY-pos[1])
+    eps=1
     withinX = rayPos[0] > 0 and rayPos[0] < maxX
     withinY = rayPos[1] > 0 and rayPos[1] < maxY
     notWall = (withinX and withinY and map[int(rayPos[1] / 10)][int(rayPos[0] / 10)] < 0)
     while(withinX and withinY and notWall):
-        toNext=0
-        toNextX=0
-        toNextY=0
-        eps=0.00001
-        if(xSlope<eps):
-            toNext=max((rayPos[1]%10)/ySlope , (10 -rayPos[1]%10)/ySlope)
-        else:
-            toNextX=max((rayPos[0]%10)/xSlope , (10 -rayPos[0]%10)/xSlope)
-        if(ySlope<eps):
-            toNext=max((rayPos[0]%10)/xSlope , (10 -rayPos[0]%10)/xSlope)
-        else:
-            toNextY=max((rayPos[1]%10)/ySlope , (10 -rayPos[1]%10)/ySlope)
-        if toNext==0:
-            toNext=min(toNextX,toNextY)
-        #update raypos to border of next grid space
-        newPos=[rayPos[0]+toNext*xSlope, rayPos[1]+toNext*ySlope,rayPos[2]]
-        eps2=0.001
-        #check if still in same grid space due to floats; if is, increment by small amount
-        while(newPos[0]/10==rayPos[0]/10 and newPos[1]/10==rayPos[1]/10):
-            newPos = [newPos[0]+eps2*xSlope, newPos[1]+eps2*ySlope,rayPos[2]]
+        #increment rayPos by eps along its direction
 
         #update rayPos
-        rayPos=newPos
+        rayPos=[rayPos[0]+xSlope*eps,rayPos[1]-ySlope*eps,rayPos[2]]
         withinX=rayPos[0]>0 and rayPos[0]<maxX
         withinY=rayPos[1]>0 and rayPos[1]<maxY
-        notWall = (withinX and withinY and map[int(rayPos[1]/10)][int(rayPos[0]/10)] < 0)
+        notWall = (withinX and withinY and
+                map[int(rayPos[1]/10)][int(rayPos[0]/10)] < 0)
 
     #return ray length, with 25cm adjustment for laser
     readingLen=math.sqrt((pos[0]-rayPos[0])**2+(pos[1]-rayPos[1])**2)
-    return adjuster(readingLen,adj,abs(lAngle))
+    finalLen=adjuster(readingLen,adj,abs(lAngle))
+    #print(finalLen,withinX,withinY,notWall)
+    return finalLen
